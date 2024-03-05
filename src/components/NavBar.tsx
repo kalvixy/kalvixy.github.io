966f6c1a76a86a018e05846f2397ab86101d39e4
Katie Fukuda <fukudakatie@gmail.com>
2024-03-04 22:38:16 -0800

    Add key to NavBar items

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -16,7 +16,7 @@ export default function NavBar() {
         <div className="flex justify-end my-4 text-3xl">
             <div className="flex space-x-4">
                 {pages.map(page => 
-                    <div className="flex w-min">
+                    <div className="flex w-min" key={page.title}>
                         <a href={page.path}>{page.title}</a>
                     </div>
                 )}
