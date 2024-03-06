64f1631ce8e4775c9753f2fb466b5cf237cfdeb0
Katie Fukuda <fukudakatie@gmail.com>
2024-03-06 00:34:14 -0800

    Update next config for gh pages and images

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
+    images: { unoptimized: true }
+};
 
 module.exports = nextConfig
